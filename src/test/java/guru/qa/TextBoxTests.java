bef289a6a737d895c56e9682cc90d655d4fc7cdb
djfrolov1996 <djfrolov1996@gmail.com>
2022-04-05 23:13:45 +0300

    added text for textforms

--- src/test/java/guru/qa/TextBoxTests.java
+++ src/test/java/guru/qa/TextBoxTests.java
@@ -1,12 +1,42 @@
 package guru.qa;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.conditions.Text;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class TextBoxTests {
+public class TextBoxTests<set> {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
     @Test
     void fillFormTest(){
-        open("https://demoqa.com/text-box");
+
+        String name = "Roman Coman";
+
+        open("/text-box");
+
+        $("[id=userName]").setValue(name);
+        $("[id=userEmail]").setValue("sss@gmail.com");
+        $("[id=currentAddress]").setValue("Moscow 33");
+        $("[id=permanentAddress]").setValue("Pushkina 22");
+        $("[id=submit]").click();
+
+
+        $("[id=output]").shouldHave(text("name"),
+                text("sss@gmail.com"),
+                text("Moscow 33"),
+                text("Pushkina 22"));
+
     }
+
 }
